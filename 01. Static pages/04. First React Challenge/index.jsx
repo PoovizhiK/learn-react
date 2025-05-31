dbdb85e0bc0c2359fda9b1717c42e41b1200bd9c
PoovizhiK <74702288+PoovizhiK@users.noreply.github.com>
2025-05-31 19:38:04 +0530

    Update index.jsx

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -1,4 +1,6 @@
-/**
+import react from react;
+import react dom from react-dom;
+ReactDOM.render(<ul>/<li>This is a new start</li><ul/>,document.getElementById("root"))/**
  * Challenge: Set up a React app from scratch again.
  * This time, try rendering an unordered list with 2-3 list items inside
  * with why you're excited to be learning React.
